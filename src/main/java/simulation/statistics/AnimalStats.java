fdb9cff48473005e7598e93a010cddc39298a085
Konrad1203 <147108244+Konrad1203@users.noreply.github.com>
2023-12-30 20:56:26 +0100

    Nie tutaj

--- src/main/java/simulation/statistics/AnimalStats.java
+++
@@ -1,5 +0,0 @@
-package simulation.statistics;
-
-public record AnimalStats(int[] genome, int activeGenome, int energyCount, int plantsEaten,
-                          int childrenCount, int descendantsCount, int daysSurvived, boolean isDead) {
-}
